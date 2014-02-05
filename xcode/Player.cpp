def8de39cec1512e25e5343539f2a120ef9b14cb
Joseph Chow <sortofsleepy@gmail.com>
2014-02-04 16:58:25 -0800

    update gitignore

---
+++ xcode/Player.cpp
@@ -0,0 +1,9 @@
+//
+//  Player.cpp
+//  PunchOut
+//
+//  Created by Joseph Chow on 2/4/14.
+//
+//
+
+#include "Player.h"
---
+++ xcode/Player.h
@@ -0,0 +1,20 @@
+//
+//  Player.h
+//  PunchOut
+//
+//  Created by Joseph Chow on 2/4/14.
+//
+//
+
+#ifndef __PunchOut__Player__
+#define __PunchOut__Player__
+
+#include <iostream>
+#include "cinder/TriMesh.h"
+
+class Player{
+public:
+    Player();
+};
+
+#endif /* defined(__PunchOut__Player__) */
